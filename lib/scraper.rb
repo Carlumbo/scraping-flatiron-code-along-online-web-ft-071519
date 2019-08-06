65d7cb8b44e386e0cb886f50145b4dd209308c8e
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-06 03:41:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,11 @@ class Scraper
   
   def get_page
    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc.css("post").each do |post|
+      course = Course.new 
+      course.title = post.css("h2")
+      course.schedule = post.css(".date")
+      course.description = post.css("p").text
  
   end 
   
